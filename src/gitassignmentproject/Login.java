39f28188202f1cd896eed9d0c07df8401b2296dc
Thando Clifford <thandomtsweni@7@gmail.com>
2026-05-19 15:24:53 +0200

    Added Login feature class

---
+++ src/gitassignmentproject/Login.java
@@ -0,0 +1,11 @@
+package gitassignmentproject;
+
+public class Login {
+
+    public static void main(String[] args) {
+
+        System.out.println("Login feature has been added successfully.");
+
+    }
+
+}
