1835b50cca97845d7f3a35e863f0a6d5861dc0da
Thando Clifford <thandomtsweni@7@gmail.com>
2026-05-19 15:36:30 +0200

    Edited Main.java in main branch

--- src/gitassignmentproject/Main.java
+++ src/gitassignmentproject/Main.java
@@ -4,8 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello NetBeans. Welcome to version control using Git");
-
+     System.out.println("This message was edited in the main branch.");
     }
 
 }
